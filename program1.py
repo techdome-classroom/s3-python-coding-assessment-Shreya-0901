33c30f6671f28687b38146d48add39db9f1c9e71
Shreya-0901 <146718466+Shreya-0901@users.noreply.github.com>
2024-10-07 16:35:46 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -4,7 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        stack = []
+
+        for char in s:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        
+        return not stack
+
+solution = Solution()
+print(solution.isValid("()"))      
+print(solution.isValid("()[]{}"))  
+print(solution.isValid("(]")) 
+pass
 
 
 
